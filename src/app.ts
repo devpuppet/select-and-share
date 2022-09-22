7a80f620ed61fa01443ccc8f57ded04901b6f604
Kamil <qkiz288@gmail.com>
2022-09-22 12:14:21 +0200

    Display place on map using OpenLayers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,42 @@
-const form = document.querySelector('form');
-const addressInput = document.getElementById('address') as HTMLInputElement;
+import TileLayer from 'ol/layer/Tile';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+
+const searchform = document.getElementById('search')!;
+const myPlacesForm = document.getElementById('my-home')!;
+const xCoord = document.getElementById('x-coord') as HTMLInputElement;
+const yCoord = document.getElementById('y-coord') as HTMLInputElement;
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
-    const enteredAddress = addressInput.value;
+    const x = +xCoord.value;
+    const y = +yCoord.value;
 
-    // send to Google API
+    const coordinates = {lat: x, lng: y};
+    drawMap(coordinates)
 }
 
-form?.addEventListener('submit', searchAddressHandler);
+function myPlacesHandler(event: Event) {
+    event.preventDefault();
+    const coordinates = {lat: 50.043529763768404, lng: 19.99406263575861};
+    drawMap(coordinates);
+}
+
+function drawMap(coordinates: {lng: number, lat: number}) {
+    document.getElementById('map')!.innerHTML = '';
+    new Map({
+        target: 'map',
+        layers: [
+            new TileLayer({source: new OSM()})
+        ],
+        view: new View({
+            center: fromLonLat([coordinates.lng, coordinates.lat]),
+            zoom: 16
+        })
+    });
+}
+
+searchform.addEventListener('submit', searchAddressHandler);
+myPlacesForm.addEventListener('submit', myPlacesHandler);
